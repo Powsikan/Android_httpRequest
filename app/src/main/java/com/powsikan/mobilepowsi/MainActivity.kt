5e203c23b34644c14ae26792c3077644f8587865
powsokan <powsi1997@gmail.com>
2020-01-28 12:24:36 +0530

    android http request

---
+++ app/src/main/java/com/powsikan/mobilepowsi/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.powsikan.mobilepowsi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        var i=1
+        var button: Button = findViewById(R.id.button_one)
+        var textView: TextView = findViewById(R.id.text_one)
+
+        button.setOnClickListener {
+
+            Toast.makeText(this,"button clicked $i", Toast.LENGTH_SHORT).show()
+
+            text_one.text = "hello $i"
+            i++
+
+
+            if (i==5){
+                var intent= Intent(this,SecondActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/powsikan/mobilepowsi/SecondActivity.kt
@@ -0,0 +1,54 @@
+package com.powsikan.mobilepowsi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+
+
+class SecondActivity : AppCompatActivity() {
+    lateinit var textView:TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        var button:Button=findViewById(R.id.button_one)
+        button.setOnClickListener {
+            httpRequest()
+
+        }
+
+      textView=findViewById(R.id.text_one)
+
+
+    }
+
+
+
+    fun httpRequest() {
+
+        val httpAsync = "https://jsonplaceholder.typicode.com/todos/1"
+            .httpGet()
+            .responseString { request, response, result ->
+                when (result) {
+                    is Result.Failure -> {
+                        val ex = result.getException()
+                        println(ex)
+                    }
+                    is Result.Success -> {
+                        val data = result.get()
+                        println(data)
+                        textView.text=data
+                    }
+                }
+            }
+
+        httpAsync.join()
+    }
+
+}
